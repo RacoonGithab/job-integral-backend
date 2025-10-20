ba2428be0d00ddfd1fd12433c4ac475cb04ec5ee
Bohdan Anokhin <rengungicko@gmail.com>
2025-10-20 19:41:50 +0300

    chore: remove test directory from repository and ignore in git

--- tests/passwordBcryptHash.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-async function hashPassword(password: string) {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    console.log(hashedPassword);
-}
-
-hashPassword('Tensam');
